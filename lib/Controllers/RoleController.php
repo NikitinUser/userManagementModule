ad334323acf4e0652eec2e8e0841afafcef671e7
NikitinUser <jo.kent@mail.ru>
2021-10-02 02:53:26 +0500

    Add controllers.

---
+++ lib/Controllers/RoleController.php
@@ -0,0 +1,10 @@
+<?php
+namespace NikitinUser\userManagementModule\lib\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+
+}
