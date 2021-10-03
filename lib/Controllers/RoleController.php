16f77e3162c2da832df2ad52ba8dd2019b719422
NikitinUser <jo.kent@mail.ru>
2021-10-04 00:47:23 +0500

    Add basic logic to controllers.

--- lib/Controllers/RoleController.php
+++ lib/Controllers/RoleController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
+    public function getPageAllRoles()
+    {
+        return view('user-management-module::allRoles');
+    }
+
+    public function getPageAddRole()
+    {
+        return view('user-management-module::addRole');
+    }
+
+    public function getPageEditRole()
+    {
+        return view('user-management-module::editRole');
+    }
+
+    public function addRole()
+    {
+        
+    }
+
+    public function editRole()
+    {
+        
+    }
+
+    public function daleteRole()
+    {
+        
+    }
+
+    public function changePermissionRole()
+    {
+        
+    }
 }
--- lib/Controllers/UserManagementController.php
+++ lib/Controllers/UserManagementController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class UserManagementController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
+    public function getPageAllUsers()
+    {
+        return view('user-management-module::allUsers');
+    }
+
+    public function getPageAddUser()
+    {
+        return view('user-management-module::addNewUser');
+    }
+
+    public function getPageUserInfo()
+    {
+        return view('user-management-module::userInfo');
+    }
+
+    public function addUser()
+    {
+        
+    }
+
+    public function removeUser()
+    {
+        
+    }
+
+    public function changeRoleUser()
+    {
+        
+    }
 }
