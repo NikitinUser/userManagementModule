7eb6cf46a10743d347bddb6b16733aa28e5ce0d3
NikitinUser <jo.kent@mail.ru>
2021-10-17 04:04:45 +0500

    Add all roles functional

--- lib/Models/Role.php
+++ lib/Models/Role.php
@@ -2,8 +2,30 @@
 namespace NikitinUser\userManagementModule\lib\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Role extends Model
 {
     protected $table = 'roles';
+
+    public function addRole($roleData)
+    {
+        $this->insert($roleData);
+    }
+
+    public function getAll()
+    {
+        return $this->select()
+             ->get()
+             ->toArray();
+    }
+
+    public function deleteRole($roleId)
+    {
+        DB::table('permissions_for_role')
+                ->where('id_role', '=', $roleId)
+                ->delete();
+
+        $this->where('id', $roleId)->delete();
+    }
 }
